af3d91f2e727968e35af449bccf9ea68fd339cec
huddy0 <hlo45475@uga.edu>
2024-02-10 15:29:53 -0500

    Functional Artist Name and Popularity Search

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from requests import post
 import json
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import random
+import pprint
 
 # Authorization using .env variables. Spotify API key must be added to .env file.
  
@@ -12,11 +14,6 @@ load_dotenv()
 
 client_id = os.getenv("dbc6f743e2ce4a1d93c7a2b00d0f6837")
 client_secret = os.getenv("a6df35cdef845d2b18876b5bf7362b2")
-
-# Authentication
-
-# print(client_id)
-# print(client_secret)
 
 client_credentials_manager = SpotifyClientCredentials(client_id="dbc6f743e2ce4a1d93c7a2b00d0f6837", client_secret="fa6df35cdef845d2b18876b5bf7362b2")
 sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
@@ -26,21 +23,12 @@ playlist_URI = playlist_link.split("/")[-1].split("?")[0]
 track_name = [x["track"]["name"] for x in sp.playlist_tracks(playlist_URI)["items"]]
 track_uris = [x["track"]["uri"] for x in sp.playlist_tracks(playlist_URI)["items"]]
 
-input = input()
-result = sp.search(input)
+def searchArtist(artist):
+    result = sp.search(artist, limit=1,type = 'artist', market="US")
+    for t in result['artists']['items']:
+        pprint.pprint(f"{t['name']} + {t['popularity']}")
 
-for idx, track in enumerate(result['tracks']['items']):
-    print(idx, track['name'])
-
-
-# # Goes through playlist and prints name along with danceability score
-# for track in sp.playlist_tracks(playlist_URI)["items"]:
-#     name = track["track"]["name"]
-#     energy = sp.audio_features(track["track"]["uri"])[0]["danceability"]
-#     popularity = track["track"]["popularity"]
-
-#     print(f"{name}  -  {energy}  -  {popularity}")
-
-
-
-
+print("\n Welcome to the Spotify Game \n")
+print(f"Input Artist: ", end ="")
+artist = input()
+searchArtist(artist)
